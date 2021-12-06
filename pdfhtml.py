7b0b3d53226fc3bea53d496b22659089bb0b185a
Ramakrishna Gundala <ramakrishna.gundala@semanticbits.com>
2021-12-06 15:50:39 +0000

    dd

--- pdfhtml.py
+++ pdfhtml.py
@@ -3,4 +3,4 @@ css = CSS(string='''
     @page {size: A4; margin: 1cm;} 
     th, td {border: 1px solid black;}
     ''')
-HTML('test.html').write_pdf('weasyprint_pdf_report.pdf', stylesheets=[css])
+HTML('test.html').write_pdf('weasyprint_pdf_repor222t.pdf', stylesheets=[css])
